954d83b2a0fe492c7120b59e5d7fefe75f4d339e
Renê Antunes Ladeira <93138911+Rene-Antunes@users.noreply.github.com>
2023-12-05 13:57:35 -0300

    feat: adding a display history

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import './Display.css'
 
 export default props =>
-    <div className="display">{props.value} </div>
+        <div className="display">
+            
+            <div className="display1">{props.value} </div>
+
+            <div className="display2"> {props.value}</div>
+
+        </div>
+       
         
    
