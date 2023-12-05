862ad41b69d2d13a9547200aa8d8be832b2cae3b
Renê Antunes Ladeira <93138911+Rene-Antunes@users.noreply.github.com>
2023-12-05 14:03:55 -0300

    feat: undoing change

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -2,13 +2,6 @@ import React from "react";
 import './Display.css'
 
 export default props =>
-        <div className="display">
-            
-            <div className="display1">{props.value} </div>
-
-            <div className="display2"> {props.value}</div>
-
-        </div>
-       
+    <div className="display">{props.value} </div>
         
    
